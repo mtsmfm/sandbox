c7102d6409fbb64f66738c6e9161be20d8a4cc77
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-04-06 23:57:58 +0900

    wip

---
+++ ruby/serverspec_docker/spec/example_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+RSpec.describe do
+  around do |example|
+    container = Docker::Container.create(
+      'Cmd'        => ['/usr/lib/systemd/systemd'],
+      'Image'      => 'gentoo-systemd',
+      'Detach'     => true,
+      'HostConfig' => {
+        'CapAdd' => ['SYS_AdMIN'],
+        'Binds'  => %w(
+           /home/mtsmfm/.ghq/github.com/esminc/esm-overlay/dev-util/drone:/var/db/repos/esm/dev-util/drone
+           /usr/portage/distfiles:/usr/portage/distfiles
+        )
+      }
+    )
+    container.start!
+
+    container.exec %w(ebuild /var/db/repos/esm/dev-util/drone/drone-9999.ebuild merge)
+
+    example.run
+
+    container.kill!
+  end
+
+  it {
+    binding.pry
+  }
+end
